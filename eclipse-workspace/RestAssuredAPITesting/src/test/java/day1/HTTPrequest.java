1ecb2a782f925b7f5054e3da9c487fb371cd0ad5
sheshma <sheshma.jaganathan@gmail.com>
2024-07-30 11:30:36 -0700

    Rest API testing

---
+++ eclipse-workspace/RestAssuredAPITesting/src/test/java/day1/HTTPrequest.java
@@ -0,0 +1,89 @@
+package day1;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.response.ValidatableResponseOptions;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+
+public class HTTPrequest {
+	int id;
+@Test(priority=1)
+	void getUsers()
+	{
+	given()
+	
+	.when()
+	     .get("https://reqres.in/api/users?page=2")
+	.then()
+	    .statusCode(200)
+	    .body("page", equalTo(2))
+	    .log().all();
+	}
+@Test(priority=2)
+void createUser()
+{
+	
+	HashMap data= new HashMap();
+	data.put("name", "sheshma");
+	data.put("job", "QA");
+	
+	id=given()
+	   .contentType("application/json")
+	   .body(data)
+	   
+	.when()
+	    .post("https://reqres.in/api/user")
+	    .jsonPath().getInt("id");
+	    
+	//.then()
+	 //    .statusCode(201)
+	 //    .log().all();
+	
+}
+
+@Test(priority=3,dependsOnMethods= {"createUser"})
+	void updateuser()
+	{
+	HashMap data=new HashMap();
+	data.put("name","john");
+	data.put("job","student");
+	
+	
+	given()
+		.contentType("application/json")
+		.body(data)
+		
+	.when()
+		.put("https://reqres.in/api/users/"+id)
+			
+	.then()
+		.statusCode(200)
+		.log().all();
+	
+}
+	@Test(priority=4)
+	void deleteuser()
+	{
+		given()
+		.when()
+		   .delete("https://reqres.in/api/users/\"+id")
+		.then()
+		    .statusCode(204)
+		    .log().all();
+}
+	private ValidatableResponseOptions<ValidatableResponse, Response> then() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	}
+
+
+
